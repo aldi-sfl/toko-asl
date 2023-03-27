036e1aadb5f898189dcf27a1e425f0b9fdb0d838
Aldi <20sa1132@amikompurwokert.ac.id>
2023-03-28 06:37:02 +0700

    second

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="css\stylecrud.css">
     <link rel="shortcut icon" type="png" href="img/logo/web icon.png" />
 
-    <title>@yield('web-title') ASL shop</title>
+    <title>@yield('web-title') ASL Shop</title>
     <script src="https://cdn.lordicon.com/ritcuqlt.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link href="https://api.fontshare.com/v2/css?f[]=satoshi@400&display=swap" rel="stylesheet"> 
